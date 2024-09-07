05726add0dfe70b85c04bf67d197c59f6c564db8
DuduRecPE <134967870+DuduRecPE@users.noreply.github.com>
2024-09-06 21:50:44 -0300

    Imersão Alura-Gemini

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos [idade da Rayssa], ela já é considerada uma das maiores skatistas de sua geração.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate sk8 fada"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Tóquio. Com sua técnica impecável e determinação, ela conquistou a medalha de ouro e inspirou uma nova geração de atletas.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza_(judoca)",
+        tags: "judô judo judoka"
+    }
+];
